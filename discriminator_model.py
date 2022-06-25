77d6eafe11dd5bed8a772d8a444594bab410e486
benyael15 <benyael15@gmail.com>
2022-06-25 11:22:08 +0300

    Create discriminator_model.py

---
+++ discriminator_model.py
@@ -0,0 +1,216 @@
+from keras.models import Sequential
+from keras.layers.merge import concatenate
+from keras.models import Model
+from keras.layers import Dense, Dropout, BatchNormalization, Conv2D, MaxPool2D, Flatten, Input
+import time
+import os
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import tensorflow_datasets as tfds
+from keras.datasets import cifar10
+from sklearn.metrics import classification_report, confusion_matrix
+from keras.utils import np_utils
+
+def discriminator_model():
+    # building a linear stack of layers with the sequential model
+    categorical_input = Input(shape=(1), name='categorical_lbl')
+    dense1 = Dense(8, activation='relu')(categorical_input)
+
+    inputL = Input(shape=(32, 32, 3), name='image')
+    conv1 = Conv2D(50, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu')(inputL)
+    conv2 = Conv2D(75, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu')(conv1)
+    maxP1 = MaxPool2D(pool_size=(2, 2))(conv2)
+    bn = BatchNormalization()(maxP1)
+    do1 = Dropout(0.25)(bn)
+    conv3 = Conv2D(125, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu')(do1)
+    maxP2 = MaxPool2D(pool_size=(2, 2))(conv3)
+    do2 = Dropout(0.25)(maxP2)
+    dense2 = Dense(32, activation='relu')(do2)
+    flatten2 = Flatten()(dense2)
+
+    merge = concatenate([dense1, flatten2])
+
+    dense3 = Dense(1028, activation='relu')(merge)
+    dropout1 = Dropout(0.4)(dense3)
+    dense4 = Dense(514, activation='relu')(dropout1)
+    dropout2 = Dropout(0.4)(dense4)
+    dense5 = Dense(256, activation='sigmoid')(dropout2)
+    dropout3 = Dropout(0.4)(dense5)
+    dense6 = Dense(128, activation='sigmoid')(dropout3)
+    dropout4 = Dropout(0.4)(dense6)
+    output = Dense(1, activation='sigmoid')(dropout4)
+
+    discriminator2 = Model(inputs=[inputL, categorical_input], outputs=output)
+    # compiling the sequential model
+    discriminator2.compile(loss='binary_crossentropy', metrics=['accuracy'], optimizer='adam')
+    discriminator2.summary()
+    return discriminator2
+
+def load_noisy_dataset():
+    noise_file = np.load('cifar-10-100n-main/data/CIFAR-10_human.npy', allow_pickle=True)
+    clean_label = noise_file.item().get('clean_label')
+    worst_label = noise_file.item().get('worse_label')
+    aggre_label = noise_file.item().get('aggre_label')
+    random_label1 = noise_file.item().get('random_label1')
+    random_label2 = noise_file.item().get('random_label2')
+    random_label3 = noise_file.item().get('random_label3')
+
+    classes_name = {0: 'airplane',
+                    1: 'automobile',
+                    2: 'bird',
+                    3: 'cat',
+                    4: 'deer',
+                    5: 'dog',
+                    6: 'frog',
+                    7: 'horse',
+                    8: 'ship',
+                    9: 'truck'}
+    return noise_file
+
+
+def load_cifar():
+    # The noisy label matches with following tensorflow dataloader
+    # train_ds, test_ds = tfds.load('cifar10', split=['train', 'test'], as_supervised=True, batch_size=-1)
+    # train_images, train_labels = tfds.as_numpy(train_ds)
+    # test_images, test_labels = tfds.as_numpy(test_ds)
+
+    (X_train, y_train), (X_test, y_test) = cifar10.load_data()
+    X_train = X_train.astype('float32')
+    X_test = X_test.astype('float32')
+
+    # normalizing the data to help with the training
+    X_train /= 255
+    X_test /= 255
+
+    # one-hot encoding using keras' numpy-related utilities
+    n_classes = 10
+    y_train = np_utils.to_categorical(y_train, n_classes)
+    y_test = np_utils.to_categorical(y_test, n_classes)
+
+    return X_train, y_train, X_test, y_test
+
+
+def train_model(model, X_train, y_train, X_test, y_test):
+    history = model.fit(X_train, y_train, batch_size=16,
+                        epochs=50,
+                        validation_data=(X_test, y_test),
+                        verbose=True)
+
+    return model, history
+
+
+def plot_conf_mat(conf_mat):
+    fig, ax = plt.subplots(figsize=(4, 4))
+    ax = sns.heatmap(conf_mat,
+                     annot=True,
+                     cbar=False,
+                     fmt='d')
+    plt.xlabel("Predicted label")
+    plt.ylabel("Actual label")
+    plt.show()
+
+
+def plot_model_results(model, history, X_test, y_test):
+    model.evaluate(X_test, y_test)
+    plt.plot(history.history['val_accuracy'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'validation'], loc='upper left')
+    plt.show()
+
+    plt.plot(history.history['accuracy'])
+    plt.show()
+
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'validation'], loc='upper left')
+    plt.show()
+
+
+def create_discrimination_dataset(X_train, Y_train, X_test, Y_test):
+    New_Train_Data_i = []
+    New_Train_Data_l = []
+    New_Train_Label = []
+
+    New_Test_Data_i = []
+    New_Test_Data_l = []
+    New_Test_Label = []
+
+    noise_file = load_noisy_dataset()
+
+    for i, data in enumerate(X_train):
+        img_data = X_train[i]
+        x = tf.stack(img_data)
+        y_clean = list(Y_train[i]).index(1)
+        New_Train_Label.append(1)
+        New_Train_Data_i.append(x)
+        New_Train_Data_l.append(y_clean)
+        for noise in ['clean_label', 'worse_label', 'aggre_label', 'random_label1', 'random_label2', 'random_label3']:
+            noise_label = noise_file.item().get(noise)[i]
+            if y_clean != noise_label:
+                x = tf.stack(img_data)
+                y = noise_label
+                New_Train_Label.append(0)
+                New_Train_Data_i.append(x)
+                New_Train_Data_l.append(y)
+
+    for i, data in enumerate(X_test):
+        img_data = X_test[i]
+        x = tf.stack(img_data)
+        y_clean = list(Y_test[i]).index(1)
+        New_Test_Label.append(1)
+        New_Test_Data_i.append(x)
+        New_Test_Data_l.append(y_clean)
+        for noise in ['clean_label', 'worse_label', 'aggre_label', 'random_label1', 'random_label2', 'random_label3']:
+            noise_label = noise_file.item().get(noise)[i]
+            if y_clean != noise_label:
+                x = tf.stack(img_data)
+                y = noise_label
+                New_Test_Label.append(0)
+                New_Test_Data_i.append(x)
+                New_Test_Data_l.append(y)
+
+    x_TrI = np.stack(New_Train_Data_i)
+    x_TrI = np.array(x_TrI)
+    x_TeI = np.stack(New_Test_Data_i)
+    x_TeI = np.array(x_TeI)
+
+    x_TrL = np.stack(New_Train_Data_l).reshape(len(New_Train_Data_l), 1)
+    x_TrL = np.array(x_TrL)
+    x_TeL = np.stack(New_Test_Data_l).reshape(len(New_Test_Data_l), 1)
+    x_TeL = np.array(x_TeL)
+
+
+    New_Train_Label = np.array(New_Train_Label)
+    New_Test_Label = np.array(New_Test_Label)
+    print("DONE!")
+
+    return [x_TrI, x_TrL], New_Train_Label, [x_TeI, x_TeL], New_Test_Label
+
+def main():
+    # noisy_dataset = load_noisy_dataset()
+    discriminator = discriminator_model()
+    X_train, y_train, X_test, y_test = load_cifar()
+    X_train, y_train, X_test, y_test = create_discrimination_dataset(X_train, y_train, X_test, y_test)
+
+    model, history = train_model(discriminator, X_train, y_train, X_test, y_test)
+
+    y_predicted = model.predict(X_test)
+    # Get back from catagorical to lables
+    y_actual = np.argmax(y_test, axis=1)
+    print(classification_report(y_actual, y_predicted))
+    conf_mat = confusion_matrix(y_actual, y_predicted)
+    plot_conf_mat(conf_mat)
+
+
+if __name__ == '__main__':
+    start = time.time()
+    main()
+    end = time.time()
+    print(f"total time took is: {end - start}")
